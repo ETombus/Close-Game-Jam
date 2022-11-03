1d3b206f47ce4f8e61185ff7ab4a41d22e29973a
Ajdin Talic <89469945+MagmarRager@users.noreply.github.com>
2022-11-03 13:05:01 +0100

    Enemy Death done

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -4,9 +4,24 @@ using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
+    private Animator anim;
+    private Rigidbody2D rigBody;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        rigBody = GetComponent<Rigidbody2D>();
+    }
     public void Die()
+    {
+        anim.SetBool("IsDead",true);
+        rigBody.velocity = Vector3.zero;
+        Invoke(nameof(DestroyEnemy),0.5f);
+    }
+    
+    private void DestroyEnemy()
     {
         Destroy(gameObject);
     }
-
+    
 }

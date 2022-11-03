4597dd0c7e2d9716df1d763ac0d3292b8452bdde
Tom <tom.eckerstrom@hotmail.com>
2022-11-03 14:25:26 +0100

    camera

--- Assets/cameraPosition.cs
+++ Assets/cameraPosition.cs
@@ -5,9 +5,28 @@ using UnityEngine;
 public class cameraPosition : MonoBehaviour
 {
     GameObject fire;
+    GameObject player;
+
+    void Start()
+    {
+        fire = FindObjectOfType<FireHealthScript>().gameObject;
+        player = FindObjectOfType<PlayerHealthScript>().gameObject;
+    }
 
     void Update()
     {
-        
+        if(player != null)
+        {
+            transform.position = Vector3.MoveTowards(fire.transform.position, player.transform.position, 2.5f);
+            transform.position = new Vector3(transform.position.x, transform.position.y, -10f);
+        }
+        else
+        {
+            transform.position = new Vector3(fire.transform.position.x, fire.transform.position.y, -10f);
+            try
+            {
+                player = FindObjectOfType<PlayerHealthScript>().gameObject;
+            }catch{}
+        }
     }
 }

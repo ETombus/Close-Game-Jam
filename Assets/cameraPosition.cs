9c4369514c256bff60971197f2202ba0081451b3
Tom <tom.eckerstrom@hotmail.com>
2022-11-03 14:09:53 +0100

    TODO

---
+++ Assets/cameraPosition.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraPosition : MonoBehaviour
+{
+    GameObject fire;
+
+    void Update()
+    {
+        
+    }
+}

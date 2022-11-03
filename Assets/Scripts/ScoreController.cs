d9aa19eb23bbb53ad5d6a75ce3f8f4ebef917940
Tom <tom.eckerstrom@hotmail.com>
2022-11-03 16:01:22 +0100

    Score

--- Assets/Scripts/ScoreController.cs
+++ Assets/Scripts/ScoreController.cs
@@ -1,10 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreController : MonoBehaviour
 {
     [SerializeField]
     private int[] highscores = new int[5];
+    private GameStateController gameState;
+    private int currentScore;
+    public TextMeshProUGUI scoreCounter;
+
+    void Start()
+    {
+        gameState = GetComponent<GameStateController>();
+        StartCoroutine(Score());
+    }
+
+    IEnumerator Score()
+    {
+        while(gameState.currentState != GameStateController.GameState.GameOver)
+        {
+            currentScore+=1;
+            scoreCounter.text = "Score: " + currentScore.ToString("D4");
+            yield return new WaitForSeconds(1f);
+        }
+    }
 
 }
